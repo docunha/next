9396b6f5706a248e86c073ce658ce6e7c6be16b7
Rodrigo Cunha <github@docunha.com.br>
2025-05-22 16:24:01 -0300

    Add dashboard and invoices pages, layout component, and navigation links
    - Created new pages for Dashboard and Invoices with placeholder content.
    - Implemented a layout component that includes a side navigation.
    - Updated the main page to include a logo and hero images.
    - Enhanced navigation links to highlight the active link.
    - Introduced global CSS and font imports for consistent styling.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+    // <main className="flex min-h-screen flex-col p-6">
+    //   <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+    //     <h1 className="text-white text-2xl">Dashboard</h1>
+    //   </div>
+    //   <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+    //     <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+    //       <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
+    //         Welcome to the Dashboard.
+    //       </p>
+    //     </div>
+    //   </div>
+    // </main>
+     <p>Dashboard Page</p>
+  );
+}
