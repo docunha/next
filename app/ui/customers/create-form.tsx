a49ac6cff787bef2710eee35f099acd9e558e4e9
Rodrigo Cunha <github@docunha.com.br>
2025-06-02 08:55:37 -0300

    Add customer creation form with input fields for name, email, and image

---
+++ app/ui/customers/create-form.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import { useActionState } from 'react';
+
+import { createCustomer, State } from '@/app/lib/actions';
+import { Button } from '@/app/ui/button';
+import {
+  EnvelopeIcon,
+  PhotoIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+export default function Form() {
+  const initialState: State = { message: null, errors: {} };
+  // const [state, formAction] = useActionState(createCustomer, initialState);
+
+  return (
+    <form action={createCustomer}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        {/* Customer Name */}
+        <div className="mb-4">
+          <label htmlFor="customer" className="mb-2 block text-sm font-medium">
+            Choose customer
+          </label>
+          <div className="relative">
+            <div className="relative">
+              <input
+                id="name"
+                name="name"
+                type="string"
+                placeholder="Customer name"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="name-error"
+              />
+              <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+          {/* <div id="name-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div> */}
+        </div>
+
+        {/* customer email */}
+        <div className="mb-4">
+          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+            Email
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                id="email"
+                name="email"
+                type="string"
+                defaultValue=""
+                placeholder="Customer Email"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="email-error"
+              />
+              <EnvelopeIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+          {/* <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div> */}
+        </div>
+
+        {/* Image Customer */}
+        <fieldset>
+          <legend className="mb-2 block text-sm font-medium">
+            Select Image
+          </legend>
+          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+
+            <div className="relative">
+              <input
+                id="image_url"
+                name="image_url"
+                type="string"
+                defaultValue="/customers/evil-rabbit.png"
+                placeholder="/customers/evil-rabbit.png"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="image_url-error"
+              />
+              <PhotoIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+
+            {/* <div id="image_url-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.status &&
+                state.errors.status.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div> */}
+          </div>
+        </fieldset>
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/customers"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Create Customer</Button>
+      </div>
+    </form>
+  );
+}
