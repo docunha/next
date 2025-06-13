ea2b058b24cc515653badaebdc8325b1ce1ca656
Rodrigo Cunha <github@docunha.com.br>
2025-06-13 15:55:24 -0300

    Refactor SideNav and NavLinks components for improved structure and clarity

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -19,6 +19,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  // { name: 'User profile', href: '/dashboard/user', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
